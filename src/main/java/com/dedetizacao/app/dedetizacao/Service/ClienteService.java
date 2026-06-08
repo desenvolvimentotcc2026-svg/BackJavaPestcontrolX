bf886936cc3b9526cde535996d474b41aa05ea34
aa1271740@gmail.com <aa1271740@gmail.com>
2026-06-08 11:22:10 -0300

    Teste de cors 5

--- src/main/java/com/dedetizacao/app/dedetizacao/Service/ClienteService.java
+++ src/main/java/com/dedetizacao/app/dedetizacao/Service/ClienteService.java
@@ -3,6 +3,7 @@ package com.dedetizacao.app.dedetizacao.Service;
 import com.dedetizacao.app.dedetizacao.Dto.RegisterRequest;
 import com.dedetizacao.app.dedetizacao.Model.Cliente;
 import com.dedetizacao.app.dedetizacao.Model.Empresa;
+import com.dedetizacao.app.dedetizacao.Model.Funcionario;
 import com.dedetizacao.app.dedetizacao.Repository.ClienteRepository;
 import com.dedetizacao.app.dedetizacao.Repository.EmpresaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
