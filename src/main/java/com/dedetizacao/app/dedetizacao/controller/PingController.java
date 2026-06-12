95042818db6d9e474bb49a0b56294f5e76885d5e
daniel-backender <aa1271740@gmail.com>
2026-06-12 16:51:16 -0300

    Teste de liberação do cors authorization

---
+++ src/main/java/com/dedetizacao/app/dedetizacao/controller/PingController.java
@@ -0,0 +1,17 @@
+package com.dedetizacao.app.dedetizacao.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class PingController {
+
+    @GetMapping("/ping")
+    public ResponseEntity<Map<String, String>> ping() {
+        return ResponseEntity.ok(Map.of("status", "UP", "message", "PestControlX Ativo!"));
+    }
+}
