efa2a66f12bccad6fa7f874f4fe622307b8018ee
daniel-backender <aa1271740@gmail.com>
2026-06-17 04:14:26 -0300

    Teste  9

--- src/main/java/com/dedetizacao/app/dedetizacao/Repository/ClienteRepository.java
+++ src/main/java/com/dedetizacao/app/dedetizacao/Repository/ClienteRepository.java
@@ -2,7 +2,9 @@ package com.dedetizacao.app.dedetizacao.Repository;
 
 import com.dedetizacao.app.dedetizacao.Model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
+import java.util.List; 
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 
